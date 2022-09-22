858d26b636d385fe0dcf74252648e43289faf9c1
prajwelbk <prajwel.bk@gmail.com>
2022-09-22 21:17:46 +0530

    added 3 new models to the code

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print("adding model related code here")
+
+def model1():
+    print("adding code for model 1")
+
+def model2():
+    print("adding code for model 1")
+
+def model3():
+    print("adding code for model 1")
